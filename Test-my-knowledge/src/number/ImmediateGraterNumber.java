fd11d70e418d8d29d0b8f8f05ae3592f960398d7
Pankaj Kumar Jha <pankajkumar.jha@eclinicalworks.com>
2019-10-07 13:09:51 +0530

    Removed extra space

--- Test-my-knowledge/src/number/ImmediateGraterNumber.java
+++ Test-my-knowledge/src/number/ImmediateGraterNumber.java
@@ -3,18 +3,16 @@ package number;
 import java.util.Arrays;
 
 public class ImmediateGraterNumber {
+	
+	
     public static void main(String[] args) {
           int array[] = {4,5,3,2,8,3,1};
            int pos=-1;
 
            for(int i=array.length-1 ;i >0 && pos==-1; i--) {
-
                   int num  = array[i];
-
                   for(int j = i-1;j>=0 && pos==-1;j--) {
-
                         if(num > array[j]) {
-
                                int temp=array[j];
 
                                array[j]=array[i];
@@ -24,18 +22,16 @@ public class ImmediateGraterNumber {
                        pos=j+1;
 
                         }
-
                   }
 
            }
-
-           if(pos==-1)
+           if(pos==-1){
                   System.out.println("Not found");
-           else {
-                        Arrays.sort(array, pos, array.length);
-                        for(int i : array) {
-                               System.out.print(i+"");
-                        }                         
+           }else {
+                    Arrays.sort(array, pos, array.length);
+                    for(int i : array) {
+                           System.out.print(i+"");
+             }                         
            }
     }
 
