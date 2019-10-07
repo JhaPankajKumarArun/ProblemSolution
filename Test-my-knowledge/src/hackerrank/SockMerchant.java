184e70e970f8f5505977b6796b7a73f734f12cd1
Pankaj Kumar Jha <pankajkumar.jha@eclinicalworks.com>
2019-10-07 13:09:27 +0530

    Renamed and refactored file

--- Test-my-knowledge/src/hackerrank/SockMerchant.java
+++ Test-my-knowledge/src/hackerrank/SockMerchant.java
@@ -1,27 +1,54 @@
-package number;
+package hackerrank;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Scanner;
 
-public class SearchParing {
+public class SockMerchant {
 
-	public static int sockMerchant(int n, int[] ar) {
-		Arrays.sort(ar);
-		int numberOfPair=0;
-		int counter=0;
-		while(counter<ar.length-1){
-			if(ar[counter]==ar[counter+1]){
-				numberOfPair++;
-				counter++;
-			}
-			counter++;
-		}
+	// Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+        int numberOfPair=0;
+        Arrays.sort(ar);
+        for( int i=0;i<ar.length-1;i++){
+            if(ar[i]==ar[i+1]){
+                numberOfPair++;
+                i++;
+            }
+        }
         return numberOfPair;
     }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        //int []socksPile={10, 20, 20, 10, 10, 30, 50, 10, 20,50};
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 	
-	public static void main(String ar[]){	
-		int []socksPile={10, 20, 20, 10, 10, 30, 50, 10, 20,50};
-		int pair= SearchParing.sockMerchant(10, socksPile);
-		System.out.println(pair);
-	}
+	
 	
 }
